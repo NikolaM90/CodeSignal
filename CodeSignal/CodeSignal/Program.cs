00877bd37a3d9bf5eb801e5f0c1f322a93d3326e
NikolayMusatov <ukato90@gmail.com>
2021-12-21 22:52:14 +0300

    решение 4 задачи

---
+++ CodeSignal/CodeSignal/Program.cs
@@ -0,0 +1,18 @@
+﻿int solution(int[] inputArray)
+{
+    int r = inputArray[0] * inputArray[1];
+    for (int i = 0; i < inputArray.Length - 1; i++)
+    {
+        if (inputArray[i] * inputArray[i + 1] >= r)
+        {
+            r = inputArray[i] * inputArray[i + 1];
+        }
+    }
+    return r;
+
+
+}
+
+
+int[] test = { -23, 4, -3, 8, -12 };
+Console.WriteLine(solution(test));
