39205cb94b11a67e1f05f52e8cca6a0054b8b6f1
NikolayMusatov <ukato90@gmail.com>
2021-12-23 19:12:07 +0300

    закоминтировал старый код,решение 5 задачи

--- CodeSignal/CodeSignal/Program.cs
+++ CodeSignal/CodeSignal/Program.cs
@@ -1,18 +1,30 @@
-﻿int solution(int[] inputArray)
-{
-    int r = inputArray[0] * inputArray[1];
-    for (int i = 0; i < inputArray.Length - 1; i++)
-    {
-        if (inputArray[i] * inputArray[i + 1] >= r)
-        {
-            r = inputArray[i] * inputArray[i + 1];
-        }
-    }
-    return r;
+﻿//int solution(int[] inputArray)
+//{
+//    int r = inputArray[0] * inputArray[1];
+//    for (int i = 0; i < inputArray.Length - 1; i++)
+//    {
+//        if (inputArray[i] * inputArray[i + 1] >= r)
+//        {
+//            r = inputArray[i] * inputArray[i + 1];
+//        }
+//    }
+//    return r;
 
+
+//}
+
+
+//int[] test = { -23, 4, -3, 8, -12 };
+//Console.WriteLine(solution(test));
+
+
+int solution(int n)
+{
+    int r = 0;
+    for (int i = 1; i < n; i++)
+    {
+        r += (2 * i - 1) * 2;
+    }
+    return r + (2 * n - 1);
 
 }
-
-
-int[] test = { -23, 4, -3, 8, -12 };
-Console.WriteLine(solution(test));
